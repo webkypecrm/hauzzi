0fbeea9b7ece5d9c110651bc58bbef35cb5f6723
webkype <webkypecrm@gmail.com>
2025-06-19 11:09:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
+});
